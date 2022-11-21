ad525108b4e7d0a9afbb67c0e68b577c2ddad2ca
The Cheese Guy <jappiah-aberde@lincoln.edu.gh>
2022-11-21 13:57:27 +0000

    with css

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,10 +1,10 @@
-//import bgImg from '../assets/images/bgImg2.png'
+import bgImg from '../assets/images/bgImg2.png'
 
 function Banner() { /**Accept the headline from page title and fill that option */
     return (
         <section>
             <div class="gp blk-lyr opc2">
-                <div class="fxd-bg"></div>
+                <div class="fxd-bg" style={{backgroundImage: `url(${bgImg})`, height: "90vh", backgroundRepeat: "no-repeat", backgroundSize: "cover"}}></div>
                 <div class="container">
                     <div class="pg-tp text-center">
                         <h1 itemprop="headline">Applications</h1>
